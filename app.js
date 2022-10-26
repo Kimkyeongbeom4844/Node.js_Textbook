6276673fe38e43d663fd7ae25e72a0ca482e6f17
김경범 <bibix321@naver.com>
2022-10-26 18:21:14 +0900

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const fs = require('fs');
-
-http.createServer((req,res)=>{
-    console.log(req.url, req.headers.cookie);
-    res.writeHead(200, {'set-cookie':'mycookie=test'});
-    res.end('Hello cookie');
-}).listen(8080,()=>{console.log('http://localhost:8080 is running')});
